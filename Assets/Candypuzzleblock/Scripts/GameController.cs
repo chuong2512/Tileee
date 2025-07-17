3aa286a159d34a22d36bce145a36fca43037faa9
pro_c <hieutg244@gmail.com>
2025-07-17 23:47:00 +0700

    update

---
+++ Assets/Candypuzzleblock/Scripts/GameController.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using TMPro;
+
+public class GameController : Singleton<GameController> 
+{
+	public static GameMode gameMode = GameMode.CLASSIC;
+	public Canvas UICanvas; 
+	
+	public TextMeshProUGUI ResetPowerQuantityText;
+	public TextMeshProUGUI ClearPowerQuantityText;
+
+	// Checks if interner is available or not.
+	public bool isInternetAvailable()
+	{
+		if (Application.internetReachability == NetworkReachability.ReachableViaCarrierDataNetwork || Application.internetReachability == NetworkReachability.ReachableViaLocalAreaNetwork) {
+			return true;
+		}
+		return false;
+	}
+
+	/// <summary>
+	/// Quits the game.
+	/// </summary>
+	public void QuitGame()
+	{
+		Invoke ("QuitGameWithDelay", 0.5F);
+	}
+
+	/// <summary>
+	/// Quits the game with delay.
+	/// </summary>
+	void QuitGameWithDelay ()
+	{
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.Quit ();
+		#endif
+	}
+
+	public void TryUseResetPower()
+	{
+		var resetPowerQuantity = PlayerPrefs.GetInt("ResetPower");
+		if (resetPowerQuantity < 1)
+		{
+			GamePlayUI.Instance.ShowShop();
+		}
+		else
+		{
+			PlayerPrefs.SetInt("ResetPower", resetPowerQuantity - 1);
+			GamePlay.Instance.ResetShape();	
+			UpdateResetPowerQuantityText();
+		}
+	}
+
+	public void TryUseClearPower()
+	{
+		var clearPowerQuantity = PlayerPrefs.GetInt("ClearPower");
+		if (clearPowerQuantity < 1)
+		{
+			GamePlayUI.Instance.ShowShop();
+		}
+		else
+		{
+			PlayerPrefs.SetInt("ClearPower", clearPowerQuantity - 1);
+			GamePlay.Instance.ClearMap();
+			UpdateClearPowerQuantityText();
+		}
+	}
+
+	public void UpdateResetPowerQuantityText()
+	{
+		var resetPowerQuantity = PlayerPrefs.GetInt("ResetPower");
+		ResetPowerQuantityText.text = resetPowerQuantity.ToString();
+	}
+	
+	public void UpdateClearPowerQuantityText()
+	{
+		var clearPowerQuantity = PlayerPrefs.GetInt("ClearPower");
+		ClearPowerQuantityText.text = clearPowerQuantity.ToString();
+	}
+	// Use this for initialization
+	void Start () 
+	{
+		UpdateResetPowerQuantityText();
+		UpdateClearPowerQuantityText();
+	}
+}
---
+++ Assets/Candypuzzleblock/Scripts/Help/InGameHelp.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameHelp : MonoBehaviour 
+{
+	public GameObject helpObject = null;
+
+	/// <summary>
+	/// Starts the help.
+	/// </summary>
+	public void StartHelp()
+	{
+		switch (GameController.gameMode) {
+		case GameMode.CLASSIC:
+		case GameMode.CHALLENGE:
+			ShowBasicHelp ();
+			break;
+		case GameMode.BLAST:
+			StackManager.Instance.SpawnUIScreen ("Help-Bomb-Mode");
+			PlayerPrefs.SetInt ("isHelpShown_" + GameController.gameMode.ToString (), 1);
+			break;
+		case GameMode.TIMED:
+			GamePlay.Instance.timeSlider.PauseTimer ();
+			StackManager.Instance.SpawnUIScreen ("Help-TimeMode");
+			PlayerPrefs.SetInt ("isHelpShown_" + GameController.gameMode.ToString (), 1);
+			break;
+		case GameMode.ADVANCE:
+			StackManager.Instance.SpawnUIScreen ("Help-Advance-Mode");
+			PlayerPrefs.SetInt ("isHelpShown_" + GameController.gameMode.ToString (), 1);
+			break;
+		}
+
+	}
+
+	/// <summary>
+	/// Shows the basic help.
+	/// </summary>
+	public void ShowBasicHelp() { }
+
+	/// <summary>
+	/// Stops the help.
+	/// </summary>
+	public void StopHelp()
+	{
+		helpObject = StackManager.Instance.PeekWindow ();
+		StackManager.Instance.PopWindow ();
+
+		if (helpObject != null && helpObject.name.Contains("Help-Classic")) {
+			Destroy (helpObject);
+			PlayerPrefs.SetInt ("isBasicHelpShown", 1);
+		} 
+		GamePlay.Instance.isHelpOnScreen = false;
+		Destroy (this);
+	}
+}
